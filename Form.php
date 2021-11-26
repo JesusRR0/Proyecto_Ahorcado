e73d018db49683d4786cca9879a9880de583c553
jesus <9435@iespuertasdelcampo.es>
2021-11-26 10:22:18 +0100

    Fallo Funcion

--- Form.php
+++ Form.php
@@ -22,103 +22,168 @@
         //comprobacion de si intento no esta seteado, setearlo a 1 y mostrar la primera vez la imagen del primer muñeco
             if(!isset($intento)){
             $intento =1;
-            echo $intento;
-
+            
             ?> 
-                <img src="./img/ahorcado<?php echo $intento;?>.png">
+                <img src='./img/ahorcado<?php echo $intento;?>.png'>
             <?php
-        }
-        //en caso de que no este vacio el arrayError se introduciran sus valores unserializados a la variable errores 
-        if(!empty($arrayError)){
-            $errores = unserialize($arrayError);
-        }
-        
-        print_r($errores);
-        //en caso de que no este vacio el array se introduciran sus valores unserializados a la variable guion, sino se llega el array de "_" 
-        if(!empty($array)){
-            $guion = unserialize($array);
-                
-        }else{
-
-            for($i=1;$i<=strlen($adivinar);$i++){
-                $guion[$i] = '_';
-                echo $guion[$i].' ';
             }
-                        
-        }
-
-        //se accede al condicional sino esta vacia el input de entrada de datos
-        
-        if(!empty($palabraInt)){
-            //se asignan los valores returnados de la variable separarLetras y se le asignan a la variable $letras
-            $letras = separarLetras($adivinar);
             
-            //se accede a la condicion en caso de que las letras introducidas se encuentren en el array $letras   
+            //en caso de que no este vacio el arrayError se introduciran sus valores unserializados a la variable errores 
+            if(!empty($arrayError)){
+                $errores = unserialize($arrayError);
+            }
             
-            
-            if(in_array($palabraInt,$letras)){ 
-            //se guarda la posicion en la que se encuentra dentro de la palabra y en la mismo posicion del array $guion se sustituyen los guiones por cada letra 
-                $posicion = array_keys($letras,$palabraInt);
-                $guion[$posicion[0]] = $letras[$posicion[0]];
-            //se muestra la imagen de nuevo pero para actualizarla en caso de que fuese necesario
-             ?> 
-                <img src="./img/ahorcado<?php echo $intento;?>.png">
-             <?php 
-            //se muestra el array $guion entero separado por espacios
-                    foreach($guion as $valor){
-                        echo $valor." ";
-                    }
-            //en caso de que no se encuentren las letras introducidas
+            //en caso de que no este vacio el array se introduciran sus valores unserializados a la variable guion, sino se llega el array de '_' 
+            if(!empty($array)){
+                $guion = unserialize($array);
+                    
             }else{
-            //se almacenan las letras en un array aparte llamado $errores
-                $errores[] = $palabraInt;
-            //se incrementan los intentos haciendo que la imagen del muñeco cambie                
-                ++$intento;
-                ?> 
-                    <img src="./img/ahorcado<?php echo $intento;?>.png">
-                <?php 
 
-            //se vuelven a mostrar el array $guion     
-                foreach($guion as $valor){
-                    echo $valor." ";
+                for($i=1;$i<=strlen($adivinar);$i++){
+                    $guion[$i] = '_';
+                    echo $guion[$i].' ';
                 }
-                print_r($errores);
-                
-            }   
-        }
-        //funcion para separar las letras de las palabras que se deberan adivinar
-        function separarLetras($adivinar){
-            //un bucle que se ejecutara segun la longitud de la palabra
-            for($i=1;$i<=strlen($adivinar);$i++){
-                //se en una variable la distancia negativa +1 para poder quitar las letras sobrantes a la cadena
-                $longitud = -strlen($adivinar)+$i;
-                //si es 0 se perderia la ultima letra, así que se iguala a 1 cuando lo sea
-                if($longitud == 0){
-                    $longitud =+ 1;
-                }
-                //un array que guarda en cada posicion cada una de las letras de la palabra introducida
-                $letras[$i] = substr($adivinar,$contador,$longitud);
-                //un contador que nos sirve para indicar a partir de que posicion cuenta la funcion substr() en el array anterior
-                ++$contador;
-                
-                
+                            
             }
-            return $letras;
+            echo $intento;
+            if($intento<7){
+                print_r($letras);
+
+                //se accede al condicional sino esta vacia el input de entrada de datos
+                if(!empty($palabraInt)){
+                    
+                    //se asignan los valores returnados de la funcion separarLetras y se le asignan a la variable $letras
+                    echo "asdfasdf";
+                    
+                    $letras =separarLetras($adivinar);
+                    print_r($letras);
+                    
+                    //se accede a la condicion en caso de que las letras introducidas se encuentren en el array $letras   
+                    if(in_array($palabraInt,$letras)){ 
+                            echo "SI LA LETRA EXISTE ";
+                        //se guarda la posicion en la que se encuentra dentro de la palabra y en la mismo posicion del array $guion se sustituyen los guiones por cada letra 
+                            $posicion = array_keys($letras,$palabraInt);
+                            $guion[$posicion[0]] = $letras[$posicion[0]];
+                        
+                            if($intento >1){ 
+
+                                ?>
+                                    <p>
+                                        <fieldset>
+                                            <legend>Errores</legend>
+                                                <?php 
+                                                    foreach($errores as $claveError => $valorError){
+                                                        echo $valorError.', ';
+                                                    }
+                                                ?>
+                        
+                                        </fieldset>
+                                    </p>
+                                <?php
+                            }
+
+                                        ?> 
+                                <!--se muestra la imagen de nuevo pero para actualizarla en caso de que fuese necesario -->
+                                    <img src='./img/ahorcado<?php echo $intento;?>.png'>
+                                <?php 
+                                //se muestra el array $guion entero separado por espacios
+                                        foreach($guion as $valor){
+                                            echo $valor.' ';
+                                        }
+
+                                
+                    //en caso de que no se encuentren las letras introducidas
+                    }else{
+                        //se almacenan las letras en un array aparte llamado $errores
+                        if($intento <2){
+                            $errores[] = $palabraInt;
+
+                        }else{
+                            
+                            if(in_array($palabraInt,$errores)){
+                                echo "No cometas los mismos errores!!!";
+                                --$intento;
+                                echo(array_key_last($errores)+1);
+                            }else{
+                                $errores[] = $palabraInt;
+                            }
+                        
+                        }
+                        
+                        ?>
+                            <p>
+                                <fieldset>
+                                    <legend>Errores</legend>
+                                        <?php 
+                                            foreach($errores as $claveError => $valorError){
+                                                echo $valorError.',';
+                                            }
+                                        ?>
+                
+                                </fieldset>
+                            </p>
+                        <?php
+                        
+                        //se incrementan los intentos haciendo que la imagen del muñeco cambie                
+                            ++$intento;
+                            
+                            ?> 
+                                <img src='./img/ahorcado<?php echo $intento;?>.png'>
+                            <?php 
+
+                        //se vuelven a mostrar el array $guion     
+                            foreach($guion as $valor){
+                                echo $valor.' ';
+                            }
+                            
+                        
+                    }   
+                }
+
+                
+                //funcion para separar las letras de las palabras que se deberan adivinar
+                function separarLetras($adivinar){
+                    //un bucle que se ejecutara segun la longitud de la palabra
+                    for($i=1;$i<=strlen($adivinar);$i++){
+                        //se en una variable la distancia negativa +1 para poder quitar las letras sobrantes a la cadena
+                        $longitud = -strlen($adivinar)+$i;
+                        //si es 0 se perderia la ultima letra, así que se iguala a 1 cuando lo sea
+                        if($longitud == 0){
+                            $longitud =+ 1;
+                        }
+                        //un array que guarda en cada posicion cada una de las letras de la palabra introducida
+                        $letras[$i] = substr($adivinar,$contador,$longitud);
+                        //un contador que nos sirve para indicar a partir de que posicion cuenta la funcion substr() en el array anterior
+                        ++$contador;
+                        
+                        
+                    }
+                    return $letras;
+                }
+            
+                    ?>
+                    <p>
+                        <form action='#' method='post'>
+                            <input type='text' name='palabraInt' id='palabraInt' required autofocus>
+                            <input type='hidden' name='adivinar' value='<?php echo $adivinar;?>'>
+                            <input type='hidden' name='guion' value='<?php echo serialize($guion);?>' >
+                            <input type='hidden' name='intento' value='<?php echo $intento;?>'>
+                            <input type='hidden' name='errores' value='<?php echo serialize($errores);?>'>
+                            <input type='submit' value='Comprobar'>
+                        </form>
+            
+                    </p>
+                <?php
+        }else{
+            echo "HAS PERDIDO";
         }
+        ?>
 
         
-            ?>
-        <p>
-            <form action='#' method='post'>
-                <input type='text' name='palabraInt' id='palabraInt' required autofocus>
-                <input type='hidden' name='adivinar' value='<?php echo $adivinar;?>'>
-                <input type='hidden' name='guion' value='<?php echo serialize($guion);?>' >
-                <input type="hidden" name="intento" value="<?php echo $intento;?>">
-                <input type="hidden" name="errores" value="<?php echo serialize($errores);?>">
-                <input type='submit' value='Comprobar'>
-            </form>
-
-        </p>
+            
+        
+        
+        
          
         
 
